33bfb50af19ba702896d9dd09faede3257b62f4b
tzelleke <theodros.zelleke@gmail.com>
2023-06-28 10:14:48 +0200

    feat(api): adds User resource with index, show, destroy

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): UserResource
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): Response
+    {
+        $user->delete();
+
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('users', UserController::class)
+        ->only(['index', 'show', 'destroy'])
+    ;
 });
